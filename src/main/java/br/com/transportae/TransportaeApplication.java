a384fa87e4574a6f133a1e79c59f6db9da348466
Mirer Balbino de Andrade Junior <mirer.rmj@gmail.com>
2023-10-28 10:41:06 -0300

    Commit inicial

---
+++ src/main/java/br/com/transportae/TransportaeApplication.java
@@ -0,0 +1,13 @@
+package br.com.transportae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/transportae/TransportaeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.transportae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
