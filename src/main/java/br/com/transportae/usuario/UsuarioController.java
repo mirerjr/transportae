16edaf10cb8e1012ca6c76c35dbf3053c3398cec
Mirer Balbino de Andrade Junior <mirer.rmj@gmail.com>
2023-11-05 17:11:30 -0300

    Fix: adiciona import do @Valid no usuarioController

--- src/main/java/br/com/transportae/usuario/UsuarioController.java
+++ src/main/java/br/com/transportae/usuario/UsuarioController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 
 @RestController()
 @RequestMapping("/api/v1/usuarios")
