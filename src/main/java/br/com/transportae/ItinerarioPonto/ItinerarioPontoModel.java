1aed82b9dd71e79db68776ad63f4c0fd78c8dc58
Mirer Balbino de Andrade Junior <mirer.rmj@gmail.com>
2024-01-07 21:55:39 -0300

    Feat: adiciona domínios ItinerarioPontoModel e ItinerarioPontoStatusModel

---
+++ src/main/java/br/com/transportae/ItinerarioPonto/ItinerarioPontoModel.java
@@ -0,0 +1,48 @@
+package br.com.transportae.ItinerarioPonto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import br.com.transportae.ItinerarioPontoStatus.ItinerarioPontoStatusModel;
+import br.com.transportae.itinerario.ItinerarioModel;
+import br.com.transportae.pontoParada.PontoParadaModel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity(name = "itinerario_ponto")
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItinerarioPontoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @CreationTimestamp
+    private LocalDateTime dataCadastro;
+
+    @UpdateTimestamp
+    private LocalDateTime dataAtualizacao;
+
+    @ManyToOne
+    private ItinerarioModel itinerario;
+
+    @ManyToOne
+    private PontoParadaModel pontoParada;
+
+    @OneToMany(mappedBy = "itinerarioPonto")
+    List<ItinerarioPontoStatusModel> pontoStatus;
+}
--- src/main/java/br/com/transportae/ItinerarioPontoStatus/ItinerarioPontoStatusModel.java
+++ src/main/java/br/com/transportae/ItinerarioPontoStatus/ItinerarioPontoStatusModel.java
@@ -1,22 +1,17 @@
-package br.com.transportae.itinerario;
+package br.com.transportae.ItinerarioPontoStatus;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import br.com.transportae.ItinerarioPonto.ItinerarioPontoModel;
-import br.com.transportae.ItinerarioStatus.ItinerarioStatusModel;
-import br.com.transportae.linhaTransporte.LinhaTransporteModel;
+import br.com.transportae.usuario.UsuarioModel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,33 +20,30 @@ import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@Entity(name = "itinerario")
+@Entity(name = "ponto_status")
 @AllArgsConstructor
-@NoArgsConstructor
-public class ItinerarioModel {
+@NoArgsConstructor 
+public class ItinerarioPontoStatusModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(length = 10)
-    private String codigoVeiculo;
+    @Column(length = 50)
+    private String nome;
 
-    @Enumerated(EnumType.STRING)
-    TipoItinerario tipoItinerario;
+    @Column
+    private boolean confirmado;
 
     @CreationTimestamp
     private LocalDateTime dataCadastro;
 
     @UpdateTimestamp
     private LocalDateTime dataAtualizacao;
+    
+    @OneToOne
+    private ItinerarioPontoModel itinerarioPonto;
 
     @OneToOne
-    private LinhaTransporteModel linhaTransporte;
-
-    @OneToMany(mappedBy = "itinerario")
-    private List<ItinerarioStatusModel> status;
-
-    @OneToMany(mappedBy = "itinerario")
-    private List<ItinerarioPontoModel> pontosItinerario;
+    private UsuarioModel usuario;
 }
