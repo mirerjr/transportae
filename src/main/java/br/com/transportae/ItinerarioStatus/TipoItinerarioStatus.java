557260a5cef0866ba25976a07fbe1ffbb32afc74
Mirer Balbino de Andrade Junior <mirer.rmj@gmail.com>
2024-01-08 09:33:12 -0300

    Feat: complementa tipos de status do itinerário

--- src/main/java/br/com/transportae/ItinerarioStatus/TipoItinerarioStatus.java
+++ src/main/java/br/com/transportae/ItinerarioStatus/TipoItinerarioStatus.java
@@ -2,4 +2,9 @@ package br.com.transportae.ItinerarioStatus;
 
 public enum TipoItinerarioStatus {
     MOTORISTA_ESPERA,
+    INICIADO,
+    EM_ATRASO,
+    FINALIZADO,
+    CANCELADO,
+    ACIDENTE,
 }
