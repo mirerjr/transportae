e85f66dbd6e2b2c8a39e1469c43e4d6b771f789b
Mirer Balbino de Andrade Junior <mirer.rmj@gmail.com>
2024-01-08 09:41:28 -0300

    Feat: substitui 'confirmado' de ItinerarioPontoStatus por enum TipoItinerarioPontoStatus

--- src/main/java/br/com/transportae/ItinerarioPontoStatus/ItinerarioPontoStatusModel.java
+++ src/main/java/br/com/transportae/ItinerarioPontoStatus/ItinerarioPontoStatusModel.java
@@ -9,6 +9,8 @@ import br.com.transportae.ItinerarioPonto.ItinerarioPontoModel;
 import br.com.transportae.usuario.UsuarioModel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -32,8 +34,8 @@ public class ItinerarioPontoStatusModel {
     @Column(length = 50)
     private String nome;
 
-    @Column
-    private boolean confirmado;
+    @Enumerated(EnumType.STRING)
+    private TipoItinerarioPontoStatus status;
 
     @CreationTimestamp
     private LocalDateTime dataCadastro;
---
+++ src/main/java/br/com/transportae/ItinerarioPontoStatus/TipoItinerarioPontoStatus.java
@@ -0,0 +1,9 @@
+package br.com.transportae.ItinerarioPontoStatus;
+
+public enum TipoItinerarioPontoStatus {
+    ALUNO_AUSENTE,
+    ALUNO_PRESENTE,
+    ALUNO_DESMARCOU,
+    MOTORISTA_CONFIRMOU_PARADA,
+    MOTORISTA_DESMARCOU_PARADA,
+}
