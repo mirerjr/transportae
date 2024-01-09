ade1bff64a39c8d217dd9645e480f5d3fafb446d
Mirer Balbino de Andrade Junior <mirer.rmj@gmail.com>
2024-01-09 14:55:28 -0300

    Feat: adiciona o Eureka Client

--- src/main/java/br/com/transportae/TransportaeApplication.java
+++ src/main/java/br/com/transportae/TransportaeApplication.java
@@ -2,8 +2,10 @@ package br.com.transportae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TransportaeApplication {
 
 	public static void main(String[] args) {
